cd7b771b5a157237def7a2d350c7f41309f60cfb
Igor Demin <i.demin@hotmail.com>
2023-11-09 09:54:13 +0500

    done next 4 tasks

---
+++ TASKS/06-data-types/finish.js
@@ -0,0 +1,33 @@
+/** ЗАДАЧА 6 - Типы данных
+ *
+ * 1. Объявите несколько переменных и присвойте им значения:
+ *  - строка
+ *  - число
+ *  - логическое
+ *  - null
+ *  - undefined
+ *  - объект
+ *  - массив
+ *
+ * 2. Выведите в консоль тип каждого из значений
+ * используя оператор typeof
+ */
+
+const myString = 'string'
+const myNumber = 123
+const myLogical = true
+const myNull = null
+const myUndefined = undefined
+const myObject = {
+    name: 'igor',
+    age: 37
+}
+const myArray = [1, 2, 3]
+
+console.log(typeof myString)
+console.log(typeof myNumber)
+console.log(typeof myLogical)
+console.log(typeof myNull)
+console.log(typeof myUndefined)
+console.log(typeof myObject)
+console.log(typeof myArray)
--- index.js
+++ index.js
@@ -1,9 +1,3 @@
-function a() {
-    console.log('Hello World')
-  }
-  
- a() // 'Hello World'
-  
-a = 10
-  
- a() // Uncaught TypeError: a is not a function
+function myFn(a) {
+    console.log(a);
+}
